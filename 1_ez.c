c07a00da6eb17b7e5d79c285f790f5edbe8b98e2
Tomasmusicmann05 <87447449+Tomasmusicmann05@users.noreply.github.com>
2021-11-18 10:18:06 -0300

    entregado

--- 1_ez.c
+++ 1_ez.c
@@ -1,3 +1,13 @@
 // escribir un programa que diga "Hola, x" donde x
 // es un argumento (string)
-// usar la funcion saludar de libprueba.h
+// usar la funcion saludar  string(99,98,97) de libprueba.h
+#include <stdlib.h>
+#include "libprueba.h"
+
+int main(int argc, char *argv[]) {
+
+}
+return 0;
+
+void saludar(char *name);
+return 0;
